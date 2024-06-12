adb7a596d12ec18e86e4dace4985eee5e23cef1f
Gorox <gnikita.010@gmail.com>
2024-06-12 18:54:18 +0300

    add editUser, add history push

--- task1/usersService/src/controllers/user.js
+++ task1/usersService/src/controllers/user.js
@@ -1,11 +1,14 @@
 const sequelize = require('sequelize');
 
 const User = require('../models/user');
-const { emailRegex } = require('../utils');
+const { emailRegex, getChangedProps } = require('../utils');
+const { pushHistory } = require('../historyApi');
 
 const logger = require('log4js').getLogger('CTRL:USER');
 logger.level = 'debug';
 
+// валидатор для того, чтобы сильно не повторять код валидации
+// возможно для тестового задания излишне, но почему бы и нет
 const validator = {
     name: (name) => {
         return name && name.length > 1 && name.length < 15
@@ -15,6 +18,10 @@ const validator = {
     },
     email: (email) => {
         return email.test(emailRegex);
+    },
+    // id должен быть десериализован до проверки
+    id: (id) => {
+        return !isNaN(id) && id >= 0 && id <= 2147483647 // postgres serial minmax
     }
 }
 
@@ -47,9 +54,19 @@ async function createUser(req, res) {
     }
 
     try {
-        await User.create({
-            firstName: name, secondName: surname, email
+        // создаём пользователя
+        const user = await User.create({
+            firstName: name, lastName: surname, email
         });
+
+        // успешно запушить историю не обязательно для успешного запроса (?)
+        try {
+            // логгируем создание в историю
+            await pushHistory('CREATE', user.id, null);
+        } catch (error) {
+            logger.error(error);
+            logger.warn('^^^ Error while pushing user CREATE');
+        }
 
         return res.json({
             success: true
@@ -66,6 +83,93 @@ async function createUser(req, res) {
 
 async function editUser(req, res) {
     logger.debug('editUser');
+
+    const name = req.body.name;
+    if (!validator.name(name)) {
+        return res.status(400).json({
+            success: false,
+            error: 'Invalid name'
+        });
+    }
+
+    const surname = req.body.surname;
+    if (!validator.surname(surname)) {
+        return res.status(400).json({
+            success: false,
+            error: 'Invalid surname'
+        });
+    }
+
+    const email = req.body.email;
+    if (!validator.email(email)) {
+        return res.status(400).json({
+            success: false,
+            error: 'Invalid email'
+        });
+    }
+
+    const uid = req.params.id;
+    if(!validator.id(uid)){
+        return res.status(400).json({
+            success: false,
+            error: 'Invalid user id'
+        });
+    }
+
+    try {
+        const user = await User.findByPk(uid);
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                error: 'User not found!'
+            });
+        }
+
+        const newUserValues = {
+            firstName: name,
+            lastName: surname,
+            email
+        }
+
+        const updatedProps = getChangedProps(user, newUserValues, ['firstName', 'lastName', 'email']);
+        if(!updatedProps.length){
+            logger.warn(`User (id${user.id}) on update, but nothing changed`);
+
+            return res.json({
+                success: true
+            });
+        }
+
+        // пока не обновили, создадим объект для истории [{key,oldValue,newValue}...]
+        const historyData = updatedProps.map(prop => {
+            return {
+                key: prop,
+                oldValue: user[prop],
+                newValue: newUserValues[prop]
+            }
+        });
+
+        await user.update(newUserValues);
+
+        // запушим историю уже после обновления
+        try {
+            await pushHistory('EDIT', user.id, historyData);
+        } catch (error) {
+            logger.error(error);
+            logger.warn('^^^ Error while pushing user EDIT');
+        }
+
+        return res.json({
+            success: true
+        });
+    } catch (error) {
+        logger.error(error);
+
+        return res.status(500).json({
+            success: false,
+            error: 'Internal server error'
+        });
+    }
 }
 
 async function getUsers(req, res) {
