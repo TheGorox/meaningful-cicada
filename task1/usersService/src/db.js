ce80b8d32effc6d3cc5896d8f133f2565974e68b
Gorox <gnikita.010@gmail.com>
2024-06-12 21:31:11 +0300

    fix db: one-line key does not work anymore

--- task1/usersService/src/db.js
+++ task1/usersService/src/db.js
@@ -1,5 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PG_TOKEN);
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
 
 module.exports = sequelize
