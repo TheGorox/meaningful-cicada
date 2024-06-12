9ebd8924cf213218c3a9be6c692a39d0d3ea5ca4
Gorox <gnikita.010@gmail.com>
2024-06-12 21:25:23 +0300

    fix dotenv: now it's working

--- task1/usersService/src/dotenv.js
+++ task1/usersService/src/dotenv.js
@@ -1,16 +1,17 @@
 // модуль для активации .env файла по всему окружению
 const path = require('node:path');
+const fs = require('node:fs');
 
 const logger = require('log4js').getLogger('DOTENV');
 logger.level = 'warn';
 
 try {
     const dotenv = require('dotenv');
-    
-    const envFilePath = path.join(__dirname, '.env');
-    dotenv.config({
-        path: envFilePath
-    });
+
+    const envConfig = dotenv.parse(fs.readFileSync(path.join(__dirname, '../.env')));
+    for (var k in envConfig) {
+        process.env[k] = envConfig[k]
+    }
 } catch (error) {
     logger.error(error);
     logger.warn('^^^ Failed to load dotenv');
